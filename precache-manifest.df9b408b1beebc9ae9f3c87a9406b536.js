c25de5f9add8d60c44f7af42080e37d0f60ea81e
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-09-18 19:03:18 +0800

    Updates

---
+++ precache-manifest.df9b408b1beebc9ae9f3c87a9406b536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef851d8834e62ee3fbf883935e09e3b5",
+    "url": "/github.io/Employee-directory/index.html"
+  },
+  {
+    "revision": "6063e4961923fc8970a2",
+    "url": "/github.io/Employee-directory/static/css/main.f6a42cf4.chunk.css"
+  },
+  {
+    "revision": "7d2b7f908a74656e7499",
+    "url": "/github.io/Employee-directory/static/js/2.f4382383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/Employee-directory/static/js/2.f4382383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6063e4961923fc8970a2",
+    "url": "/github.io/Employee-directory/static/js/main.673218e2.chunk.js"
+  },
+  {
+    "revision": "6cc30769d38464e6c08d",
+    "url": "/github.io/Employee-directory/static/js/runtime-main.0cd5b1e4.js"
+  }
+]);
