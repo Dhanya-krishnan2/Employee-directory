fd6b6273acc172a52b62cb5bfc66b0f7994dd51f
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-05-23 17:48:11 +0800

    adding changes to the design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="m-10 text-sm text-center text-gray-500">
+        <div className="m-10 text-sm text-center text-red-500">
             <p>Employee pictures provided by <a href="https://randomuser.me/photos" className="font-bold text-gray-600 hover:text-yellow-500">Randomuser.Me</a></p>
             <p>Built by <a href="https://github.com/Dhanya-krishnan2?tab=repositories/" className="font-bold text-gray-600 hover:text-yellow-500"> <b><i>DHANYA KUNHIKRISHNAN</i></b></a></p>
         </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-function Layout(props) {
+function Layout(props) { 
+    // this is the layout of the middle section of  the
+    // so we are distributing all the details inside the middle container as children
+     console.log(props.children)
+
     return (
         <div className="container max-w-6xl px-10 mx-auto">
             {props.children}
